cc792cfae40f52d95d15fa926e75d7c78fb43519
Marco Schumacher <marco@schumacher.dev>
2023-01-23 21:57:00 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    '@pvtnbr/eslint-config',
+  ],
+
+  rules: {
+    indent: ['error', 2],
+    'unicorn/filename-case': ['error', { case: 'camelCase' }],
+  },
+
+  overrides: [
+    {
+      files: '*.{json,json5,jsonc}',
+      rules: {
+        'jsonc/indent': ['error', 2],
+      },
+    },
+  ],
+};
