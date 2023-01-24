846484a152a483a30c7e1c14a40e798a658fe094
Marco Schumacher <marco@schumacher.dev>
2023-01-24 16:50:44 +0100

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,41 @@
 module.exports = {
-  extends: [
-    '@pvtnbr/eslint-config',
-  ],
+  extends: ['@pvtnbr/eslint-config'],
 
   rules: {
     indent: ['error', 2],
-    '@typescript-eslint/indent': ['error', 2],
     'unicorn/filename-case': ['error', { case: 'camelCase' }],
   },
 
   overrides: [
+    {
+      files: '*.{ts,tsx,mts,cts,vue}',
+      rules: {
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+
+    {
+      files: '*.{jsx,tsx}',
+      extends: ['plugin:react/jsx-runtime'],
+    },
+
     {
       files: '*.{json,json5,jsonc}',
       rules: {
         'jsonc/indent': ['error', 2],
       },
     },
+  ],
+
+  ignorePatterns: [
+    //
+    '**/node_modules/**',
+    '{tmp,temp}/**',
+    '**/*.min.js',
+    '**/vendor/**',
+    '**/dist/**',
+    '**/coverage/**',
   ],
 };
