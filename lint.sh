c97f56e1383ed8159034241bfe08ee659f6a46a8
Marco Schumacher <marco@schumacher.dev>
2023-01-24 22:18:33 +0100

    wip

---
+++ lint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+eslint \
+    --cache \
+    --cache-location node_modules/.cache/eslint/.eslintcache \
+    --cache-strategy content \
+    --max-warnings 0 \
+    --no-error-on-unmatched-pattern \
+    $@
