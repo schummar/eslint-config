413e6558fcfd010adbbd9629868057e0954e753e
Marco Schumacher <marco@schumacher.dev>
2023-06-14 09:04:42 +0200

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,34 @@
 module.exports = {
   extends: [
     //
-    '@pvtnbr/eslint-config',
-    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
 
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+
   rules: {
-    'unicorn/filename-case': [
-      'error',
-      {
-        case: 'camelCase',
-        ignore: [
-          //
-          /test-d\.ts$/,
-          /README\.md$/,
-        ],
-      },
-    ],
     'no-plusplus': 'off',
     'max-params': 'off',
     'func-names': 'off',
     'prefer-destructuring': 'off',
     'no-cond-assign': ['error', 'except-parens'],
-    'unicorn/no-this-assignment': 'off',
     'no-console': ['warn', { allow: ['error', 'warn', 'debug', 'info'] }],
     'import/max-dependencies': 'off',
-    'react/prop-types': 'off',
     complexity: 'off',
     'import/no-duplicates': [
       'warn',
@@ -36,13 +39,14 @@ module.exports = {
     ],
     'max-nested-callbacks': 'off',
     'node/no-unpublished-import': 'off',
-    'unicorn/no-array-method-this-argument': 'off',
+    'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
   },
 
   overrides: [
     {
       files: '*.{ts,tsx,mts,cts,vue}',
       rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         'no-useless-constructor': 'off',
         '@typescript-eslint/no-useless-constructor': 'warn',
@@ -58,18 +62,6 @@ module.exports = {
         '@typescript-eslint/no-shadow': 'off',
         '@typescript-eslint/ban-types': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
-      },
-    },
-
-    {
-      files: '*.{jsx,tsx}',
-      extends: ['plugin:react/jsx-runtime'],
-    },
-
-    {
-      files: '*.{json,json5,jsonc}',
-      rules: {
-        'jsonc/indent': ['error', 2],
       },
     },
   ],
